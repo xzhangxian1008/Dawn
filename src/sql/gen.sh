c490d629ab43468878fec197e30ea6f0b713922c
xzhangxian1008 <xzhangxian@foxmail.com>
2021-12-29 10:42:44 +0800

    tweaking

--- src/sql/gen.sh
+++ src/sql/gen.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 lex -i scanner.lex
-rm lex.yy.cpp
+if test -e "lex.yy.cpp"
+then rm lex.yy.cpp
+fi
 mv lex.yy.c lex.yy.cpp
 
 yacc parser.yy -d
-rm y.tab.cpp
+if test -e "y.tab.cpp"
+then rm y.tab.cpp
+fi
 mv y.tab.c y.tab.cpp
